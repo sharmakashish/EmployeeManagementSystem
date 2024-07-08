7c51c279fa13780cc576e36ed2bbe1d53b571729
sharmakashish <kashish.juc1@gmail.com>
2024-07-08 18:05:22 +0530

    Add a new Employee

---
+++ src/main/java/org/ems/components/CompanyHeader.java
@@ -0,0 +1,13 @@
+package org.ems.components;
+
+import org.apache.tapestry5.annotations.Property;
+
+public class CompanyHeader {
+
+    @Property
+    private String companyName = "Kane Solutions";
+
+    @Property
+    private String companyAddress = " Żebbuġ,Malta";
+
+}
